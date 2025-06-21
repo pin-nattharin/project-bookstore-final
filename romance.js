a54cca13436ef75c503fab74e5e34ac44ee03b97
pin-nattharin <nattharin3004@gmail.com>
2025-06-22 00:49:26 +0700

    alert add to cart

--- romance.js
+++ romance.js
@@ -32,26 +32,30 @@ document.addEventListener('DOMContentLoaded', () => {
         <div class="card-body">
           <h5 class="card-title">${book.name}</h5>
           <p class="card-text text-danger">$${book.price}</p>
-          <button class="btn btn-primary w-100 add-to-cart" data-id="${book.id}" data-name="${book.name}" data-price="${book.price}">
-            Add to Cart
+          <button class="btn btn-primary w-100 add-to-cart" 
+          data-id="${book.id}" 
+          data-name="${book.name}" 
+          data-price="${book.price}">
+          Add to Cart
           </button>
         </div>
       </div>
-    `;
+      `;
+        
 
     container.appendChild(col);
   });
 
-  document.addEventListener('click', (e) => {
-    if (e.target.classList.contains('add-to-cart')) {
-      const button = e.target;
-      const isLoggedIn = localStorage.getItem('login') === 'true';
+  //document.addEventListener('click', (e) => {
+    //if (e.target.classList.contains('add-to-cart')) {
+      //const button = e.target;
+      //const isLoggedIn = localStorage.getItem('login') === 'true';
 
-      if (!isLoggedIn) {
-        alert("Please login before adding to cart.");
+      //if (!isLoggedIn) {
+        //alert("Please login before adding to cart.");
         // เรียก modal login ได้ถ้ามี
-        return;
-      }
+       // return;
+      //}
 
       const item = {
         name: button.dataset.name,
@@ -63,5 +67,4 @@ document.addEventListener('DOMContentLoaded', () => {
       localStorage.setItem('cart', JSON.stringify(cart));
       alert(`${item.name} added to cart.`);
     }
-  });
-});
+  );
